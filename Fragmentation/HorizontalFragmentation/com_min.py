a3058f44612e5a97652be96f16c000659fc608ef
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-01-03 22:01:11 +0100

    inital

---
+++ Fragmentation/HorizontalFragmentation/com_min.py
@@ -0,0 +1,8 @@
+#input : R relation, Pr: set of simple predicates
+#output : Pr2 : set of simple predicates
+
+F = [] # set of minterm fragments
+
+Pr2 = []
+
+# find pi belongs to Pr such that pi partitions R according to Rule 1
